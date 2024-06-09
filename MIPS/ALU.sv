d898e427c44459b0dbda87d43f646adcc5245671
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2024-06-09 23:56:34 +0300

    Update ALU.sv

--- MIPS/ALU.sv
+++ MIPS/ALU.sv
@@ -1,3 +1,19 @@
+parameter OR = 4'b0000;
+parameter AND=4'b0001;
+parameter XOR=4'b0010;
+parameter SLL=4'b0011;
+parameter SRL=4'b0100;
+parameter SUB=4'b0101;
+parameter ADD=4'b0110;
+parameter NOR =4'b0111;
+parameter NAND =4'b1000;
+parameter SLT=4'b1001;
+parameter MUL=4'b1010;
+parameter CLR=4'b1011;
+parameter STF=4'b1100;
+parameter LWSW =4'b1101;
+parameter SRA=4'b1110;
+parameter ANDN=4'b1111;
 module ALU(
     input [31:0] a,
     input [31:0] b,
@@ -12,28 +28,34 @@ module ALU(
     reg overflow;
     always @(posedge clk) begin
         case (csig)
-            4'b0000: register<=(a|b);
-            4'b0001: register<=(a&b);
-            4'b0010: register<=(a^b);
-            4'b0011: register<= (a<<b);
-            4'b0100: register<=(a>>b);
-            4'b0101: begin 
-            register<=a-b;
+            OR: register<=(a|b);// or 0 
+            AND: register<=(a&b);// and 1
+            XOR: register<=(a^b);// xor 
+            SLL: register<= (a<<b);// sll
+            SRL: register<=(a>>b);//srl
+            SUB: begin 
+            register<=a-b;//sub
             overflow<=~(((register>0)^(a>0)) | ((a>0)^(b>0)));
             end
-            4'b0110: begin 
-                register<=a+b;
+            ADD: begin 
+                register<=a+b;//add 
             overflow<=~(((register>0)^(a>0)) | ((a>0)&(b>0)));
             end
-            4'b0111: register<=~(a|b);
-            4'b1000: register<=~(a&b);
-            4'b1001: register<=((a<b)?32'b1:32'b0);
-            4'b1010: register<=(a*b);
-            4'b1011:register<=32'b0;
-            4'b1100:register<=8'hffffffff;
-            4'b1101:register<=(a+(b>>1));
-            4'b1110:register<=a&~b;
-            4'b1111:register<=~(a^b);
+            NOR: register<=~(a|b);//nor
+            NAND: register<=~(a&b);//nand 
+            SLT: register<=((a<b)?32'b1:32'b0);//slt 
+            MUL: register<=(a*b);//mul
+            CLR:register<=32'b0;//clr
+            STF:register<=8'hffffffff;//stf
+            LWSW:register<=(a+(b>>1));// lw/sw
+            /* SRA*/
+            SRA:begin 
+                if(a<0 && b!=0)
+                register<=~((~a)>>b);
+                else
+                register<=a>>b; 
+            end// sra
+            ANDN:register<=a&~b;//andn
         endcase
     end
     assign v=overflow;
